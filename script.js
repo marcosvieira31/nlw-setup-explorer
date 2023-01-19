6b850d1576b4d3e4219521aaa8bb1222f20a0f32
Marcos Vieira <marcosnd.31@gmail.com>
2023-01-19 17:24:23 -0300

    Dia 3 - Javascript
    Adição de lógica de negócio através do javascript;
    Utilização da biblioteca NLWSetup;
    Adição de responsividade para 768px e 600px.

---
+++ script.js
@@ -0,0 +1,12 @@
+const form = document.querySelector('#form-habits');
+const nlwSetup = new NLWSetup(form);
+
+const data = { 
+    run: ['01-01', '01-02', '01-06', '01-08'], 
+    water: ['01-04', '01-05'],
+    workout: ['01-01', '01-03', '01-07'],
+};
+  
+nlwSetup.setData(data);
+
+nlwSetup.load();
