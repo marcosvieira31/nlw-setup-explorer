50af88175d88d64c93a1fa5c4e9d9fc8578c545e
Marcos Vieira <marcosnd.31@gmail.com>
2023-01-20 17:13:31 -0300

    Atualização do script.js
    Adição de funcionalidade de novo registro;
    Adição de funcionalidade de salvar informações no localStorage.

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const form = document.querySelector('#form-habits');
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector('header button');
 
-const data = { 
-    run: ['01-01', '01-02', '01-06', '01-08'], 
-    water: ['01-04', '01-05'],
-    workout: ['01-01', '01-03', '01-07'],
-};
-  
+button.addEventListener('click', add);
+form.addEventListener('change', save);
+
+function add() {
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5);
+    const dayExists = nlwSetup.dayExists(today);
+    
+    if(dayExists){
+        alert('Dia já incluso! ⚠️');
+        return;
+    }
+
+    nlwSetup.addDay(today);
+    alert('Dia adicionado com sucesso! ✅');    
+}
+
+function save() {
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data));
+}
+
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {};
 nlwSetup.setData(data);
-
 nlwSetup.load();
